ad50d8c165678ee7c71dafadc6999d8c338bc619
qwerty <anzor.movlatov@gmial.com>
2021-10-25 09:55:53 +0300

    Был добавлен новый класс RX_6900

--- main.py
+++ main.py
@@ -25,10 +25,16 @@ class RX_6700(AMD):
     pass
 
 
+class RX_6900(AMD):
+
+    pass
+
+
 if __name__ == '__main__':
-    rx_5500 = RX_5500("HDMI", " 8 gb", "2016 y.")
-    rx_6600 = RX_6600("VGA", "8gb", "2018 y.")
-    rx_6700 = RX_6700("HDMI", "12gb", "2020 y")
-    jobs = [rx_5500, rx_6600, rx_5500]
+    rx_5500 = RX_5500("HDMI", " 8 gb", "2016 year")
+    rx_6600 = RX_6600("VGA", "8gb", "2018 year")
+    rx_6700 = RX_6700("HDMI", "12gb", "2020 year")
+    rx_6900 = RX_6900("DVI","16gb","2021 year")
+    jobs = [rx_5500, rx_6600, rx_5500,rx_6900]
     for j in jobs:
         j.output()
